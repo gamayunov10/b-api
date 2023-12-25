ffc463a0ecc0a594f845eaa2978442aef6e8e789
Denis Gamayunov <gamaunov1911@gmail.com>
2023-12-25 16:13:09 +0300

    changed method findUsers => added pagination

--- src/features/users/infrastructure/users.query.repository.ts
+++ src/features/users/infrastructure/users.query.repository.ts
@@ -11,26 +11,46 @@ export class UsersQueryRepository {
   constructor(@InjectDataSource() private dataSource: DataSource) {}
 
   async findUsers(query: UserQueryModel) {
+    const whereConditions = [];
+    const params = [];
+
+    if (query.searchLoginTerm) {
+      whereConditions.push(`u.login ILIKE $${params.length + 1}`);
+      params.push(`%${query.searchLoginTerm}%`);
+    }
+
+    if (query.searchEmailTerm) {
+      whereConditions.push(`u.email ILIKE $${params.length + 1}`);
+      params.push(`%${query.searchEmailTerm}%`);
+    }
+
+    let whereClause = '';
+    if (whereConditions.length > 0) {
+      whereClause = 'WHERE ' + whereConditions.join(' AND ');
+    }
+
     const users = await this.dataSource.query(
-      `SELECT u.id,
-              u.login,
-              u.email,
-              u."createdAt"
+      `SELECT u.id, u.login, u.email, u."createdAt"
        FROM public.users u
+       ${whereClause}
        ORDER BY "${query.sortBy}" ${query.sortDirection}
-       LIMIT ${query.pageSize} OFFSET (${query.pageNumber} - 1) * ${query.pageSize}
-       `,
+       LIMIT $${params.length + 1} OFFSET ($${params.length + 2} - 1) * $${
+        params.length + 3
+      }`,
+      [...params, query.pageSize, query.pageNumber, query.pageSize],
     );
 
     const totalCount = await this.dataSource.query(
       `SELECT count(*)
-       FROM public.users`,
+       FROM public.users u
+       ${whereClause}`,
+      params,
     );
 
     return Paginator.paginate({
-      pageNumber: Number(query.pageNumber),
-      pageSize: Number(query.pageSize),
-      totalCount: Number(totalCount[0].count),
+      pageNumber: +query.pageNumber,
+      pageSize: +query.pageSize,
+      totalCount: +totalCount[0].count,
       items: await this.usersMapping(users),
     });
   }
