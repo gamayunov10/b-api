6eb938fb5e035c95c89bdbbff5a6d60398a8c84c
Denis Gamayunov <gamaunov1911@gmail.com>
2023-12-27 12:04:24 +0300

    removed cookieParser, add static for swagger

--- src/settings/apply-app-setting.ts
+++ src/settings/apply-app-setting.ts
@@ -3,7 +3,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
 import { customExceptionFactory } from 'src/infrastructure/exception-filters/exception.factory';
 import * as process from 'process';
-import cookieParser from 'cookie-parser';
+import { createWriteStream } from 'fs';
+import { get } from 'https';
 
 import { HttpExceptionFilter } from '../infrastructure/exception-filters/http-exception-filter';
 import { AppModule } from '../app.module';
@@ -12,7 +13,6 @@ export const APP_PREFIX = '';
 
 export const applyAppSettings = (app: INestApplication) => {
   app.enableCors();
-  app.use(cookieParser());
   app.useGlobalFilters(new HttpExceptionFilter());
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
@@ -32,6 +32,48 @@ const setAppPrefix = (app: INestApplication) => {
 
 const setSwagger = (app: INestApplication) => {
   if (process.env.ENV !== 'PRODUCTION') {
+    get(
+      `${process.env.SERVER_URL}/swagger/swagger-ui-bundle.js`,
+      function (response) {
+        response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+        console.log(
+          `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
+        );
+      },
+    );
+
+    get(
+      `${process.env.SERVER_URL}/swagger/swagger-ui-init.js`,
+      function (response) {
+        response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+        console.log(
+          `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
+        );
+      },
+    );
+
+    get(
+      `${process.env.SERVER_URL}/swagger/swagger-ui-standalone-preset.js`,
+      function (response) {
+        response.pipe(
+          createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
+        );
+        console.log(
+          `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
+        );
+      },
+    );
+
+    get(
+      `${process.env.SERVER_URL}/swagger/swagger-ui.css`,
+      function (response) {
+        response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+        console.log(
+          `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
+        );
+      },
+    );
+
     const swaggerPath = APP_PREFIX + '/swagger';
 
     const config = new DocumentBuilder()
