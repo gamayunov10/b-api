8a02a48337ba1dd0ad935c8c9894f2070947decb
Denis Gamayunov <gamaunov1911@gmail.com>
2023-12-29 09:55:05 +0300

    fixed cookieParser import

--- src/settings/apply-app-setting.ts
+++ src/settings/apply-app-setting.ts
@@ -1,9 +1,9 @@
+import * as process from 'process';
+import * as cookieParser from 'cookie-parser';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
 import { customExceptionFactory } from 'src/infrastructure/exception-filters/exception.factory';
-import * as process from 'process';
-import cookieParser from 'cookie-parser';
 
 import { HttpExceptionFilter } from '../infrastructure/exception-filters/http-exception-filter';
 import { AppModule } from '../app.module';
