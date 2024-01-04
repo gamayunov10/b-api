43dc5085f8fc2d7cf96e1b5309a750dc148be896
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-04 14:42:45 +0300

    add methods

--- test/base/managers/users.manager.ts
+++ test/base/managers/users.manager.ts
@@ -2,11 +2,21 @@ import { INestApplication } from '@nestjs/common';
 import supertest, { Response } from 'supertest';
 
 import { UsersQueryRepository } from '../../../src/features/users/infrastructure/users.query.repository';
+import {
+  basicAuthLogin,
+  basicAuthPassword,
+} from '../utils/constants/auth.constants';
+import { BlogsQueryRepository } from '../../../src/features/blogs/infrastructure/blogs.query.repository';
+import { BlogViewModel } from '../../../src/features/blogs/api/models/output/blog-view.model';
+import { PostsQueryRepository } from '../../../src/features/posts/infrastructure/posts.query.repository';
+import { PostViewModel } from '../../../src/features/posts/api/models/output/post-view.model';
 
 export class UsersTestManager {
   constructor(
     protected readonly app: INestApplication,
     private readonly usersQueryRepository: UsersQueryRepository,
+    private readonly blogsQueryRepository: BlogsQueryRepository,
+    private readonly postsQueryRepository: PostsQueryRepository,
   ) {}
 
   expectCorrectModel(createModel: any, responseModel: any) {
@@ -33,6 +43,30 @@ export class UsersTestManager {
       })
       .send(updateModel)
       .expect(204);
+  }
+
+  async createBlog(createModel: any): Promise<Response> {
+    return supertest(this.app.getHttpServer())
+      .post('/sa/blogs')
+      .auth(basicAuthLogin, basicAuthPassword)
+      .send(createModel)
+      .expect(201);
+  }
+
+  async findBlogById(id: number): Promise<BlogViewModel | null> {
+    return await this.blogsQueryRepository.findBlogById(id);
+  }
+
+  async createPostForBlog(createModel: any, blogId: number): Promise<Response> {
+    return supertest(this.app.getHttpServer())
+      .post(`/sa/blogs/${blogId}/posts`)
+      .auth(basicAuthLogin, basicAuthPassword)
+      .send(createModel)
+      .expect(201);
+  }
+
+  async findPostByPostId(id: number): Promise<PostViewModel | null> {
+    return await this.postsQueryRepository.findPostByPostId(id);
   }
 
   async login(loginModel: any): Promise<Response> {
