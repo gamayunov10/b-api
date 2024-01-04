8cda8fdcda36446815f68c8b6f1c7d1a78b956d0
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-04 14:30:12 +0300

    fixed expectCreatedUser => added UserInputModel

--- test/base/utils/functions/expect/users/expectCreatedUser.ts
+++ test/base/utils/functions/expect/users/expectCreatedUser.ts
@@ -1,12 +1,17 @@
-export const expectCreatedUser = (response, login, email) => {
+import { UserInputModel } from '../../../../../../src/features/users/api/models/input/user-input-model';
+
+export const expectCreatedUser = (
+  response,
+  createUserInput: UserInputModel,
+) => {
   expect(response.body).toHaveProperty('id');
   expect(response.body).toHaveProperty('login');
   expect(response.body).toHaveProperty('email');
   expect(response.body).toHaveProperty('createdAt');
 
   expect(response.body.id).toBeDefined();
-  expect(response.body.login).toBe(login);
-  expect(response.body.email).toBe(email);
+  expect(response.body.login).toBe(createUserInput.login);
+  expect(response.body.email).toBe(createUserInput.email);
   expect(response.body.createdAt).toBeDefined();
 };
 
