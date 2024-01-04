881f4554685b21250f82624b6092b21d97d86d82
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-04 16:31:45 +0300

    fixed BlogUpdateCommand

--- src/features/blogs/application/usecases/update-blog.usecase.ts
+++ src/features/blogs/application/usecases/update-blog.usecase.ts
@@ -1,7 +1,15 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { NotFoundException } from '@nestjs/common';
 
 import { BlogsRepository } from '../../infrastructure/blogs.repository';
 import { BlogInputModel } from '../../api/models/input/blog-input-model';
+import { BlogsQueryRepository } from '../../infrastructure/blogs.query.repository';
+import { ResultCode } from '../../../../base/enums/result-code.enum';
+import {
+  blogIdField,
+  blogNotFound,
+} from '../../../../base/constants/constants';
+import { ExceptionResultType } from '../../../../infrastructure/types/exceptions.types';
 
 export class BlogUpdateCommand {
   constructor(public blogInputModel: BlogInputModel, public blogId: string) {}
@@ -9,12 +17,37 @@ export class BlogUpdateCommand {
 
 @CommandHandler(BlogUpdateCommand)
 export class BlogUpdateUseCase implements ICommandHandler<BlogUpdateCommand> {
-  constructor(private readonly blogsRepository: BlogsRepository) {}
+  constructor(
+    private readonly blogsRepository: BlogsRepository,
+    private readonly blogsQueryRepository: BlogsQueryRepository,
+  ) {}
 
-  async execute(command: BlogUpdateCommand): Promise<boolean> {
-    return this.blogsRepository.updateBlog(
+  async execute(
+    command: BlogUpdateCommand,
+  ): Promise<ExceptionResultType<boolean>> {
+    if (isNaN(+command.blogId)) {
+      throw new NotFoundException();
+    }
+
+    const blog = await this.blogsQueryRepository.findBlogById(+command.blogId);
+
+    if (!blog) {
+      return {
+        data: false,
+        code: ResultCode.NotFound,
+        field: blogIdField,
+        message: blogNotFound,
+      };
+    }
+
+    await this.blogsRepository.updateBlog(
       command.blogInputModel,
       +command.blogId,
     );
+
+    return {
+      data: true,
+      code: ResultCode.Success,
+    };
   }
 }
