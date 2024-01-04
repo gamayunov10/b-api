fbd2a98b50f1128a3a2e76c9a5264172ea2cd723
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-04 16:29:17 +0300

    add constants

--- test/base/utils/constants/posts.constants.ts
+++ test/base/utils/constants/posts.constants.ts
@@ -6,9 +6,45 @@ export const updatedDescription = 'updated description';
 export const updatedContent = 'updated content';
 
 export const createPostInput = {
-  title: 'string',
-  shortDescription: 'string description',
-  content: 'string content',
+  title: 'title1',
+  shortDescription: 'string description1',
+  content: 'string content1',
+};
+
+export const createPostInput2 = {
+  title: 'title2',
+  shortDescription: 'string description2',
+  content: 'string content2',
+};
+
+export const createPostInput3 = {
+  title: 'title3',
+  shortDescription: 'string description3',
+  content: 'string content3',
+};
+
+export const createPostInput4 = {
+  title: 'title4',
+  shortDescription: 'string description4',
+  content: 'string content4',
+};
+
+export const createPostInput5 = {
+  title: 'title5',
+  shortDescription: 'string description5',
+  content: 'string content5',
+};
+
+export const createPostInput6 = {
+  title: 'title6',
+  shortDescription: 'string description6',
+  content: 'string content6',
+};
+
+export const createPostInput7 = {
+  title: 'title7',
+  shortDescription: 'string description7',
+  content: 'string content7',
 };
 
 export const updatedPostInput = {
