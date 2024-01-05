a08115ebf615ea6c615a8160cb4121c2d639d1fa
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-05 09:40:53 +0300

    add decorator IsNotEmptyString

--- src/features/blogs/api/models/input/blog-input-model.ts
+++ src/features/blogs/api/models/input/blog-input-model.ts
@@ -1,15 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsUrl, MaxLength } from 'class-validator';
 
+import { IsNotEmptyString } from '../../../../../infrastructure/decorators/is-not-empty-string.decorator';
+
 export class BlogInputModel {
   @ApiProperty({
     type: String,
     maxLength: 15,
   })
+  @IsNotEmptyString()
   @MaxLength(15)
   name: string;
 
   @ApiProperty({ type: String, maxLength: 500 })
+  @IsNotEmptyString()
   @MaxLength(500)
   description: string;
 
--- src/features/posts/api/models/input/post-input-model.ts
+++ src/features/posts/api/models/input/post-input-model.ts
@@ -1,19 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { MaxLength } from 'class-validator';
 
+import { IsNotEmptyString } from '../../../../../infrastructure/decorators/is-not-empty-string.decorator';
+
 export class PostInputModel {
   @ApiProperty({
     type: String,
     maxLength: 30,
   })
+  @IsNotEmptyString()
   @MaxLength(30)
   title: string;
 
   @ApiProperty({ name: 'shortDescription', type: String, maxLength: 100 })
+  @IsNotEmptyString()
   @MaxLength(100)
   shortDescription: string;
 
   @ApiProperty({ type: String, maxLength: 1000 })
+  @IsNotEmptyString()
   @MaxLength(1000)
   content: string;
 }
