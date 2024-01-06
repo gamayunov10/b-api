2ba6c0cd79e1945af2d8e01da675a8c79b9c0530
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-06 12:42:51 +0300

    fix PostQueryModel

--- src/features/posts/api/models/input/post.query.model.ts
+++ src/features/posts/api/models/input/post.query.model.ts
@@ -1,11 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
-
 import { QueryModel } from '../../../../../base/models/query.model';
 
-export class PostQueryModel extends QueryModel {
-  @ApiProperty({ required: false })
-  @IsOptional()
-  @IsString()
-  searchNameTerm: string;
-}
+export class PostQueryModel extends QueryModel {}
