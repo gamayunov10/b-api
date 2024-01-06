99286b64cffe86e1c5d941ede67adb2211f4bbe6
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-06 12:43:28 +0300

    add name

--- src/features/users/domain/user-email-confirmation.entity.ts
+++ src/features/users/domain/user-email-confirmation.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity('user_email_confirmation')
 export class UserEmailConfirmation {
   @PrimaryGeneratedColumn()
   id: number;
