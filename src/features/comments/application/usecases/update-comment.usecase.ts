c5f90ae4d53176f25b384e6d333950e42a890d6f
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-06 19:01:41 +0300

    add CommentUpdateUseCase

---
+++ src/features/comments/application/usecases/update-comment.usecase.ts
@@ -0,0 +1,65 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { ForbiddenException, NotFoundException } from '@nestjs/common';
+
+import { ResultCode } from '../../../../base/enums/result-code.enum';
+import { ExceptionResultType } from '../../../../infrastructure/types/exceptions.types';
+import {
+  commentIDField,
+  commentNotFound,
+} from '../../../../base/constants/constants';
+import { CommentsQueryRepository } from '../../infrastructure/comments.query.repository';
+import { CommentInputModel } from '../../api/models/input/comment-input.model';
+import { CommentsRepository } from '../../infrastructure/comments.repository';
+
+export class CommentUpdateCommand {
+  constructor(
+    public commentId: string,
+    public userId: string,
+    public commentInputModel: CommentInputModel,
+  ) {}
+}
+
+@CommandHandler(CommentUpdateCommand)
+export class CommentUpdateUseCase
+  implements ICommandHandler<CommentUpdateCommand>
+{
+  constructor(
+    private readonly commentsRepository: CommentsRepository,
+    private readonly commentsQueryRepository: CommentsQueryRepository,
+  ) {}
+
+  async execute(
+    command: CommentUpdateCommand,
+  ): Promise<ExceptionResultType<boolean>> {
+    if (isNaN(+command.commentId)) {
+      throw new NotFoundException();
+    }
+
+    const comment = await this.commentsQueryRepository.findComment(
+      +command.commentId,
+    );
+
+    if (!comment) {
+      return {
+        data: false,
+        code: ResultCode.NotFound,
+        field: commentIDField,
+        message: commentNotFound,
+      };
+    }
+
+    if (+command.userId !== +comment.commentatorInfo.userId) {
+      throw new ForbiddenException();
+    }
+
+    await this.commentsRepository.updateComment(
+      +command.commentId,
+      command.commentInputModel,
+    );
+
+    return {
+      data: true,
+      code: ResultCode.Success,
+    };
+  }
+}
