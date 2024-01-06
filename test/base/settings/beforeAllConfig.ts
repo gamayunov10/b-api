379d1f01662132c8b8660b52f54dee5511209a6d
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-06 18:59:00 +0300

    add commentsQueryRepository

--- test/base/settings/beforeAllConfig.ts
+++ test/base/settings/beforeAllConfig.ts
@@ -5,6 +5,7 @@ import { UsersTestManager } from '../managers/users.manager';
 import { UsersQueryRepository } from '../../../src/features/users/infrastructure/users.query.repository';
 import { BlogsQueryRepository } from '../../../src/features/blogs/infrastructure/blogs.query.repository';
 import { PostsQueryRepository } from '../../../src/features/posts/infrastructure/posts.query.repository';
+import { CommentsQueryRepository } from '../../../src/features/comments/infrastructure/comments.query.repository';
 
 import { initializeApp } from './initializeApp';
 
@@ -20,12 +21,14 @@ export async function beforeAllConfig(): Promise<{
   const usersQueryRepository = app.get(UsersQueryRepository);
   const blogsQueryRepository = app.get(BlogsQueryRepository);
   const postsQueryRepository = app.get(PostsQueryRepository);
+  const commentsQueryRepository = app.get(CommentsQueryRepository);
 
   const usersTestManager = new UsersTestManager(
     app,
     usersQueryRepository,
     blogsQueryRepository,
     postsQueryRepository,
+    commentsQueryRepository,
   );
 
   return { app, agent, usersTestManager };
