494308523990d5588555be380815d3a0510d299f
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-06 12:42:33 +0300

    add LikesInfoViewModel

---
+++ src/features/comments/api/models/output/likes-info-view.model.ts
@@ -0,0 +1,7 @@
+import { LikeStatus } from '../../../../../base/enums/like_status.enum';
+
+export class LikesInfoViewModel {
+  likesCount: number;
+  dislikesCount: number;
+  myStatus: LikeStatus;
+}
