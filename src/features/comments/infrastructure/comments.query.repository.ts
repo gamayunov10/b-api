1303ce0b10213fb34f0e85835b3a3febf1d16efc
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-06 16:56:33 +0300

    create findCommentsByPostId

--- src/features/comments/infrastructure/comments.query.repository.ts
+++ src/features/comments/infrastructure/comments.query.repository.ts
@@ -3,10 +3,47 @@ import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 
 import { CommentViewModel } from '../api/models/output/comment-view.model';
+import { Paginator } from '../../../base/pagination/_paginator';
+import { CommentQueryModel } from '../api/models/input/comment.query.model';
 
 @Injectable()
 export class CommentsQueryRepository {
   constructor(@InjectDataSource() private dataSource: DataSource) {}
+  async findCommentsByPostId(postId: number, query: CommentQueryModel) {
+    const comments = await this.dataSource.query(
+      `SELECT 
+              c.id,
+              c.content,
+              u.id as "userId",
+              u.login as "userLogin",
+              c."createdAt"
+         FROM public.comments c
+         LEFT JOIN public.users u
+         ON c."commentatorId" = u.id
+         WHERE c."postId" = $1
+         ORDER BY "${query.sortBy}" ${
+        query.sortBy !== 'createdAt' ? 'COLLATE "C"' : ''
+      } ${query.sortDirection}
+         LIMIT ${query.pageSize} OFFSET (${query.pageNumber} - 1) * ${
+        query.pageSize
+      };`,
+      [postId],
+    );
+
+    const totalCount = await this.dataSource.query(
+      `SELECT count(*)
+       FROM public.comments
+       WHERE ("postId" = $1);`,
+      [postId],
+    );
+
+    return Paginator.paginate({
+      pageNumber: query.pageNumber,
+      pageSize: query.pageSize,
+      totalCount: totalCount[0].count,
+      items: await this.commentsMapping(comments),
+    });
+  }
   async findComment(commentId: number): Promise<CommentViewModel | null> {
     const comments = await this.dataSource.query(
       `SELECT 
