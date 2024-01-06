2c5c5eec2031d56d3a525b0707a5733628e1c0be
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-06 19:01:17 +0300

    add updateBlog

--- src/features/comments/api/comments.controller.ts
+++ src/features/comments/api/comments.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  NotFoundException,
+  Param,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBasicAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CommandBus } from '@nestjs/cqrs';
 
 import { exceptionHandler } from '../../../infrastructure/exception-filters/exception.handler';
 import { ResultCode } from '../../../base/enums/result-code.enum';
@@ -8,11 +18,18 @@ import {
   commentNotFound,
 } from '../../../base/constants/constants';
 import { CommentsQueryRepository } from '../infrastructure/comments.query.repository';
+import { JwtBearerGuard } from '../../auth/guards/jwt-bearer.guard';
+import { UserIdFromGuard } from '../../auth/decorators/user-id-from-guard.guard.decorator';
+import { CommentUpdateCommand } from '../application/usecases/update-comment.usecase';
+
+import { CommentInputModel } from './models/input/comment-input.model';
+import { CommentViewModel } from './models/output/comment-view.model';
 
 @ApiTags('comments')
 @Controller('comments')
 export class CommentsController {
   constructor(
+    private commandBus: CommandBus,
     private readonly commentsQueryRepository: CommentsQueryRepository,
   ) {}
 
@@ -20,7 +37,9 @@ export class CommentsController {
   @ApiOperation({
     summary: 'Returns comment by id',
   })
-  async findCommentById(@Param('id') commentId: string) {
+  async findCommentById(
+    @Param('id') commentId: string,
+  ): Promise<CommentViewModel | void> {
     if (isNaN(+commentId)) {
       throw new NotFoundException();
     }
@@ -33,6 +52,29 @@ export class CommentsController {
         commentNotFound,
         commentIDField,
       );
+    }
+
+    return result;
+  }
+
+  @Put(':id')
+  @ApiOperation({
+    summary: 'Update existing comment by id with InputModel',
+  })
+  @ApiBasicAuth('Bearer')
+  @UseGuards(JwtBearerGuard)
+  @HttpCode(204)
+  async updateComment(
+    @Param('id') blogId: string,
+    @UserIdFromGuard() userId: string,
+    @Body() commentInputModel: CommentInputModel,
+  ): Promise<void> {
+    const result = await this.commandBus.execute(
+      new CommentUpdateCommand(blogId, userId, commentInputModel),
+    );
+
+    if (result.code !== ResultCode.Success) {
+      return exceptionHandler(result.code, result.message, result.field);
     }
 
     return result;
--- src/features/comments/infrastructure/comments.repository.ts
+++ src/features/comments/infrastructure/comments.repository.ts
@@ -2,7 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 
+import { CommentInputModel } from '../api/models/input/comment-input.model';
+
 @Injectable()
 export class CommentsRepository {
   constructor(@InjectDataSource() private dataSource: DataSource) {}
+  async updateComment(
+    commentId: number,
+    commentInputModel: CommentInputModel,
+  ): Promise<boolean> {
+    try {
+      return this.dataSource.transaction(async (): Promise<boolean> => {
+        const result = await this.dataSource.query(
+          `UPDATE public.comments 
+                  SET content = $2
+                  WHERE id = $1;`,
+          [commentId, commentInputModel.content],
+        );
+
+        return result[1] === 1;
+      });
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
 }
