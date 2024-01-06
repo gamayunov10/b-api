818ad99267d39f2ccc882347d23a3cd2971090b7
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-06 17:31:20 +0300

    removed unused imports

--- src/features/blogs/api/blogs.controller.ts
+++ src/features/blogs/api/blogs.controller.ts
@@ -4,19 +4,16 @@ import {
   NotFoundException,
   Param,
   Query,
-  UseGuards,
 } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
-import { ApiBasicAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { BlogsRepository } from '../infrastructure/blogs.repository';
 import { BlogsQueryRepository } from '../infrastructure/blogs.query.repository';
 import { PostsQueryRepository } from '../../posts/infrastructure/posts.query.repository';
-import { BasicAuthGuard } from '../../auth/guards/basic-auth.guard';
 import { exceptionHandler } from '../../../infrastructure/exception-filters/exception.handler';
 import { ResultCode } from '../../../base/enums/result-code.enum';
 import { blogIdField, blogNotFound } from '../../../base/constants/constants';
-import { PostQueryModel } from '../../posts/api/models/input/post.query.model';
 
 import { SABlogQueryModel } from './models/input/sa-blog.query.model';
 import { BlogQueryModel } from './models/input/blog.query.model';
