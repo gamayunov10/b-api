b41d888bff4ce3f99ce731bd3ce24e9aca40efd4
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-06 18:59:12 +0300

    add createCommentInput2

--- test/base/utils/constants/comments.constant.ts
+++ test/base/utils/constants/comments.constant.ts
@@ -1,5 +1,9 @@
-import { lorem30 } from './lorems';
+import { lorem30, lorem50 } from './lorems';
 
 export const createCommentInput = {
   content: lorem30,
+};
+
+export const createCommentInput2 = {
+  content: lorem50,
 };
