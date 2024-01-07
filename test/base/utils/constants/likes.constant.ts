69d21cd9583a6c87180d8a4a5a5d0becce96593a
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-07 12:07:17 +0300

    add constants

---
+++ test/base/utils/constants/likes.constant.ts
@@ -0,0 +1,13 @@
+export const likeStatusInput_none = {
+  likeStatus: 'None',
+};
+export const likeStatusInput_dislike = {
+  likeStatus: 'Dislike',
+};
+export const likeStatusInput_like = {
+  likeStatus: 'Like',
+};
+
+export const likeStatusInput_like_lowerCase = {
+  likeStatus: 'like',
+};
