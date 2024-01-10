a1ff2452a671d363cf568e1e4ada56ac1f1f7c86
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-10 16:19:14 +0300

    fixed TokenParserMiddleware

---
+++ src/infrastructure/middlewares/token-parser.middleware.ts
@@ -0,0 +1,24 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Response } from 'express';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class TokenParserMiddleware implements NestMiddleware {
+  constructor(private readonly jwtService: JwtService) {}
+  use(req, res: Response, next: NextFunction) {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (token) {
+      const decodedToken = this.jwtService.decode(token);
+
+      console.log('TokenParserMiddleware userId:', decodedToken.userId);
+      if (!decodedToken) {
+        next();
+        return;
+      }
+
+      req.userId = decodedToken.userId;
+    }
+    next();
+  }
+}
