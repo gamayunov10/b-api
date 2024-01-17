5ade55e99a6f936e3aa282d909dc0ad14987b2aa
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-17 19:09:00 +0300

    add AppDataSource

---
+++ src/settings/app-data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
