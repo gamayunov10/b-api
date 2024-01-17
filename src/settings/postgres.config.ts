4f3c1aab93721ac82f431618a3402f5528bdc77e
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-17 19:08:42 +0300

    add entities, logging, logger

--- src/settings/postgres.config.ts
+++ src/settings/postgres.config.ts
@@ -1,6 +1,16 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as process from 'process';
 
+import { Blog } from '../features/blogs/domain/blog.entity';
+import { CommentLike } from '../features/comments/domain/comment-like.entity';
+import { Comment } from '../features/comments/domain/comment.entity';
+import { DeviceAuthSessions } from '../features/devices/domain/device.entity';
+import { Post } from '../features/posts/domain/post.entity';
+import { PostLike } from '../features/posts/domain/post-like.entity';
+import { UserEmailConfirmation } from '../features/users/domain/user-email-confirmation.entity';
+import { UserPasswordRecovery } from '../features/users/domain/user-password-recovery.entity';
+import { User } from '../features/users/domain/user.entity';
+
 export const postgresConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
@@ -9,8 +19,20 @@ export const postgresConfig: TypeOrmModuleOptions = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
   autoLoadEntities: false,
-  synchronize: false,
-
+  synchronize: true,
+  logging: 'all',
+  logger: 'debug',
+  entities: [
+    User,
+    Blog,
+    CommentLike,
+    Comment,
+    DeviceAuthSessions,
+    Post,
+    PostLike,
+    UserEmailConfirmation,
+    UserPasswordRecovery,
+  ],
   ssl: true,
   extra: {
     ssl: {
