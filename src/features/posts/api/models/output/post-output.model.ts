3820402eab78a9bf73b1e37e2c8f8ff0b0a21089
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-24 12:00:23 +0300

    add PostOutputModel

---
+++ src/features/posts/api/models/output/post-output.model.ts
@@ -0,0 +1,17 @@
+import { LikeStatus } from '../../../../../base/enums/like_status.enum';
+
+export class PostOutputModel {
+  id: string;
+  title: string;
+  shortDescription: string;
+  content: string;
+  blogId: Date;
+  blogName: boolean;
+  createdAt: boolean;
+  extendedLikesInfo: {
+    likesCount: number;
+    dislikesCount: number;
+    myStatus: LikeStatus;
+    newestLikes: [];
+  };
+}
