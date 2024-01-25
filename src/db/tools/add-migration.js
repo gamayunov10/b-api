6e2d26ec8e0040b269cf0bd00cbe83a74e8241c4
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-25 18:00:54 +0300

    delete migration config inside src

--- src/db/tools/add-migration.js
+++
@@ -1,7 +0,0 @@
-import execSync from 'child_process';
-
-const arg = process.argv[2];
-if (!arg) throw new Error('Pass the name for migration');
-const command = `typeorm-ts-node-commonjs migration:generate -d ./src/db/data-source.ts ./src/db/migrations/${arg}`;
-
-execSync(command, { stdio: 'inherit' });
