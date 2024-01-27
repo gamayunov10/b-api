9ae703dcb12a4e7f0985dee6b0a93cae21111d5f
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-27 20:16:02 +0300

    add custom decorator IsArrayNotEmpty

--- src/features/quiz/api/models/input/question-input-model.ts
+++ src/features/quiz/api/models/input/question-input-model.ts
@@ -1,8 +1,8 @@
-import { ArrayNotEmpty, IsArray, Length } from 'class-validator';
+import { Length } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
 
 import { IsNotEmptyString } from '../../../../../infrastructure/decorators/is-not-empty-string.decorator';
+import { IsArrayNotEmpty } from '../../../../../infrastructure/decorators/is-not-empty-array.decorator';
 
 export class QuestionInputModel {
   @ApiProperty({
@@ -19,14 +19,6 @@ export class QuestionInputModel {
     description:
       'All variants of possible correct answers for current questions Examples: [6, "six", "шесть", "дофига"] In Postgres save this data in JSON column',
   })
-  @IsArray()
-  @ArrayNotEmpty()
-  @Transform(({ value }) => {
-    if (!Array.isArray(value)) {
-      return false;
-    } else {
-      return value.map((a) => a.toString().trim());
-    }
-  })
+  @IsArrayNotEmpty()
   correctAnswers: string[];
 }
