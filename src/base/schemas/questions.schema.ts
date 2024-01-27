34e5c5c53d2cc186c1b041152c9296abbc80229e
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-27 20:15:11 +0300

    create QuestionsSchema for documentation

---
+++ src/base/schemas/questions.schema.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+import { QuestionViewModel } from '../../features/quiz/api/models/output/question-view-model';
+
+import { PaginatorSchema } from './paginator.schema';
+
+export class QuestionsSchema extends PaginatorSchema {
+  @ApiProperty({
+    type: QuestionViewModel,
+  })
+  'items': QuestionViewModel[];
+}
