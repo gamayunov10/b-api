f647e8a1b1feafc11ee7ca9bf69b8586f9e86bd1
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-27 20:13:52 +0300

    fix QueryModel

--- src/base/models/query.model.ts
+++ src/base/models/query.model.ts
@@ -5,19 +5,27 @@ import { QueryParamsEnum } from '../enums/query-params.enum';
 import { SortDirection } from '../enums/sort-direction.enum';
 
 export class QueryModel {
-  @ApiProperty({ required: false })
+  @ApiProperty({
+    required: false,
+    default: QueryParamsEnum.createdAt,
+    enum: QueryParamsEnum,
+  })
   @IsOptional()
   sortBy = QueryParamsEnum.createdAt;
 
-  @ApiProperty({ required: false, enum: SortDirection })
+  @ApiProperty({
+    required: false,
+    default: SortDirection.DESC,
+    enum: SortDirection,
+  })
   @IsOptional()
   sortDirection = SortDirection.DESC;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: false, default: 1 })
   @IsOptional()
   pageNumber = 1;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: false, default: 10 })
   @IsOptional()
   pageSize = 10;
 }
