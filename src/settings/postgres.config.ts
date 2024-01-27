2f4b4c4b40baf9470182a2a40cf8e51a254c428a
Denis Gamayunov <gamaunov1911@gmail.com>
2024-01-27 14:28:07 +0300

    fix entities import

--- src/settings/postgres.config.ts
+++ src/settings/postgres.config.ts
@@ -1,6 +1,20 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as process from 'process';
 
+import { UserPasswordRecovery } from '../features/users/domain/user-password-recovery.entity';
+import { Blog } from '../features/blogs/domain/blog.entity';
+import { CommentLike } from '../features/comments/domain/comment-like.entity';
+import { Comment } from '../features/comments/domain/comment.entity';
+import { DeviceAuthSessions } from '../features/devices/domain/device.entity';
+import { Post } from '../features/posts/domain/post.entity';
+import { PostLike } from '../features/posts/domain/post-like.entity';
+import { UserEmailConfirmation } from '../features/users/domain/user-email-confirmation.entity';
+import { User } from '../features/users/domain/user.entity';
+import { QuizAnswer } from '../features/quiz/domain/quiz-answer.entity';
+import { QuizQuestion } from '../features/quiz/domain/quiz-question.entity';
+import { QuizGame } from '../features/quiz/domain/quiz-game.entity';
+import { QuizPlayer } from '../features/quiz/domain/quiz-player';
+
 export const postgresConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
@@ -12,7 +26,21 @@ export const postgresConfig: TypeOrmModuleOptions = {
   synchronize: false,
   logging: 'all',
   logger: 'debug',
-  entities: ['src/**/*/.entity{.ts, .js}'],
+  entities: [
+    User,
+    Blog,
+    CommentLike,
+    Comment,
+    DeviceAuthSessions,
+    Post,
+    PostLike,
+    UserEmailConfirmation,
+    UserPasswordRecovery,
+    QuizAnswer,
+    QuizGame,
+    QuizPlayer,
+    QuizQuestion,
+  ],
 
   // ssl: true,
   // extra: {
--- typeorm.config.ts
+++ typeorm.config.ts
@@ -2,6 +2,20 @@ import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
 import process from 'process';
 
+import { User } from './src/features/users/domain/user.entity';
+import { Blog } from './src/features/blogs/domain/blog.entity';
+import { CommentLike } from './src/features/comments/domain/comment-like.entity';
+import { Comment } from './src/features/comments/domain/comment.entity';
+import { DeviceAuthSessions } from './src/features/devices/domain/device.entity';
+import { Post } from './src/features/posts/domain/post.entity';
+import { PostLike } from './src/features/posts/domain/post-like.entity';
+import { UserEmailConfirmation } from './src/features/users/domain/user-email-confirmation.entity';
+import { UserPasswordRecovery } from './src/features/users/domain/user-password-recovery.entity';
+import { QuizAnswer } from './src/features/quiz/domain/quiz-answer.entity';
+import { QuizGame } from './src/features/quiz/domain/quiz-game.entity';
+import { QuizPlayer } from './src/features/quiz/domain/quiz-player';
+import { QuizQuestion } from './src/features/quiz/domain/quiz-question.entity';
+
 config();
 
 export default new DataSource({
@@ -12,6 +26,20 @@ export default new DataSource({
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
   synchronize: false,
-  entities: ['src/**/*.entity.ts'],
+  entities: [
+    User,
+    Blog,
+    CommentLike,
+    Comment,
+    DeviceAuthSessions,
+    Post,
+    PostLike,
+    UserEmailConfirmation,
+    UserPasswordRecovery,
+    QuizAnswer,
+    QuizGame,
+    QuizPlayer,
+    QuizQuestion,
+  ],
   migrations: ['migrations/*.ts'],
 });
