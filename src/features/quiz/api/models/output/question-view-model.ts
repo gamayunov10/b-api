8b5a45b259389555a1151729f7dc622a592730ec
Denis Gamayunov <gamaunov1911@gmail.com>
2024-02-02 09:18:19 +0300

    fix QuestionViewModel

--- src/features/quiz/api/models/output/question-view-model.ts
+++ src/features/quiz/api/models/output/question-view-model.ts
@@ -3,41 +3,12 @@ import { ApiProperty } from '@nestjs/swagger';
 export class QuestionViewModel {
   @ApiProperty({
     type: String,
-    nullable: true,
   })
   id: string;
 
   @ApiProperty({
     type: String,
-    nullable: true,
-    description:
-      'Text of question, for example: How many continents are there?',
+    description: 'Here is the question itself',
   })
   body: string;
-
-  @ApiProperty({
-    type: String,
-    nullable: true,
-    description:
-      'All variants of possible correct answers for current questions Examples: [6, "six", "шесть", "дофига"] In Postgres save this data in JSON column',
-  })
-  correctAnswers: string[] | [];
-
-  @ApiProperty({
-    type: Boolean,
-    default: false,
-    description: 'If question is completed and can be used in the Quiz game',
-  })
-  published: boolean;
-
-  @ApiProperty({
-    type: Date,
-  })
-  createdAt: Date;
-
-  @ApiProperty({
-    type: Date,
-    nullable: true,
-  })
-  updatedAt: Date;
 }
