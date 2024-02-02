df98c3c4cb128adac7018cee9a5ed043c2947a89
Denis Gamayunov <gamaunov1911@gmail.com>
2024-02-02 09:16:26 +0300

    add ApiProperties

--- src/features/quiz/api/models/output/answer-view-model.ts
+++ src/features/quiz/api/models/output/answer-view-model.ts
@@ -1,7 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { AnswerStatuses } from '../../../../../base/enums/answer-statuses';
 
 export class AnswerViewModel {
+  @ApiProperty({
+    type: String,
+  })
   questionId: string;
+
+  @ApiProperty({
+    type: AnswerStatuses,
+    enum: AnswerStatuses,
+  })
   answerStatus: AnswerStatuses;
+
+  @ApiProperty({
+    type: Date,
+  })
   addedAt: Date;
 }
