d67279340896b06534a266cea6f134b3d614c665
Denis Gamayunov <gamaunov1911@gmail.com>
2024-02-02 09:17:58 +0300

    add ApiProperties

--- src/features/quiz/api/models/output/player-view-model.ts
+++ src/features/quiz/api/models/output/player-view-model.ts
@@ -1,4 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class PlayerViewModel {
+  @ApiProperty({
+    type: String,
+  })
   id: string;
+
+  @ApiProperty({
+    type: String,
+  })
   login: string;
 }
