a6e30a364283b708a2f2da5dc92c7e35901c1f23
Denis Gamayunov <gamaunov1911@gmail.com>
2024-03-01 17:07:49 +0300

    add configService, findBlogWallpaperImageRecord, findBlogMainImages, findBlogImages, blogImagesMapping

--- src/features/blogs/infrastructure/blogs.query.repository.ts
+++ src/features/blogs/infrastructure/blogs.query.repository.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
 
 import { BlogViewModel } from '../api/models/output/blog-view.model';
 import { Paginator } from '../../../base/pagination/_paginator';
@@ -11,14 +12,21 @@ import { BlogOwnerStatus } from '../../../base/enums/blog-owner.enum';
 import { BlogWithOwnerViewModel } from '../api/models/output/blog-with-owner-view.model';
 import { IFindBlogsWithBanInfoSelect } from '../api/models/select/find-blogs-with-ban-info.select';
 import { IBlogsSelect } from '../api/models/select/blogs.select';
+import { BlogImagesViewModel } from '../api/models/output/blog-images-view.model';
+import { BlogWallpaperImage } from '../domain/blog-wallpaper-image.entity';
 
 @Injectable()
 export class BlogsQueryRepository {
+  private readonly logger = new Logger(BlogsQueryRepository.name);
+  private configService: ConfigService;
   constructor(
     @InjectRepository(Blog)
     private readonly blogsRepository: Repository<Blog>,
     @InjectDataSource() private dataSource: DataSource,
-  ) {}
+    configService: ConfigService,
+  ) {
+    this.configService = configService;
+  }
 
   async findBlogs(query: BlogQueryModel) {
     const filter = blogsFilter(query.searchNameTerm);
@@ -196,6 +204,104 @@ export class BlogsQueryRepository {
     }
 
     return blog;
+  }
+
+  async findBlogWallpaperImageRecord(
+    blogId: number,
+  ): Promise<BlogWallpaperImage | boolean> {
+    try {
+      return await this.dataSource
+        .createQueryBuilder()
+        .from(BlogWallpaperImage, 'bwi')
+        .where(`bwi.blogId = :blogId`, { blogId })
+        .getOne();
+    } catch (e) {
+      if (this.configService.get('ENV') === 'DEVELOPMENT') {
+        this.logger.error(e);
+      }
+      return false;
+    }
+  }
+
+  async findBlogMainImages(
+    blogId: number,
+  ): Promise<BlogImagesViewModel | boolean> {
+    try {
+      const blogs = await this.blogsRepository
+        .createQueryBuilder('b')
+        .leftJoinAndSelect('b.blogMainImages', 'bmi')
+        .where(`b.id = :blogId`, {
+          blogId,
+        })
+        .getMany();
+
+      const mappedBlogs = await this.blogMainImagesMapping(blogs);
+      return mappedBlogs[0];
+    } catch (e) {
+      if (this.configService.get('ENV') === 'DEVELOPMENT') {
+        this.logger.error(e);
+      }
+      return false;
+    }
+  }
+
+  async findBlogImages(blogId: number): Promise<BlogImagesViewModel | boolean> {
+    try {
+      const blogs = await this.blogsRepository
+        .createQueryBuilder('b')
+        .leftJoinAndSelect('b.blogWallpaperImage', 'bwi')
+        .leftJoinAndSelect('b.blogMainImages', 'bmi')
+        .where(`b.id = :blogId`, {
+          blogId,
+        })
+        .getMany();
+
+      const mappedBlogs = await this.blogImagesMapping(blogs);
+      return mappedBlogs[0];
+    } catch (e) {
+      if (this.configService.get('ENV') === 'DEVELOPMENT') {
+        this.logger.error(e);
+      }
+      return false;
+    }
+  }
+
+  private async blogImagesMapping(blogs: Blog[]) {
+    return blogs.map((b) => {
+      const wallpaperImage = {
+        url: process.env.S3_DOMAIN + b.blogWallpaperImage.url,
+        width: Number(b.blogWallpaperImage.width),
+        height: Number(b.blogWallpaperImage.height),
+        fileSize: Number(b.blogWallpaperImage.size),
+      };
+
+      return {
+        wallpaper: wallpaperImage,
+        main: b.blogMainImages.map((bmi) => {
+          return {
+            url: process.env.S3_DOMAIN + bmi.url,
+            width: Number(bmi.width),
+            height: Number(bmi.height),
+            fileSize: Number(bmi.size),
+          };
+        }),
+      };
+    });
+  }
+
+  private async blogMainImagesMapping(blogs: Blog[]) {
+    return blogs.map((b) => {
+      return {
+        main: b.blogMainImages.map((bmi) => {
+          return {
+            url: process.env.S3_DOMAIN + bmi.url,
+            width: Number(bmi.width),
+            height: Number(bmi.height),
+            fileSize: Number(bmi.size),
+          };
+        }),
+      };
+    });
   }
 
   private async blogsMapping(array: IBlogsSelect[]): Promise<BlogViewModel[]> {
