e5b5629c8cebe535460a6e44f8ae23a195e3d586
Denis Gamayunov <gamaunov1911@gmail.com>
2024-03-01 16:57:31 +0300

    create BlogAddMainImageUseCase

---
+++ src/features/blogs/application/usecases/blog-add-main-image.usecase.ts
@@ -0,0 +1,83 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import sharp from 'sharp';
+
+import { ExceptionResultType } from '../../../../infrastructure/types/exceptions.types';
+import { ResultCode } from '../../../../base/enums/result-code.enum';
+import {
+  blogIdField,
+  blogNotFound,
+} from '../../../../base/constants/constants';
+import { BlogsQueryRepository } from '../../infrastructure/blogs.query.repository';
+import { S3Adapter } from '../../../../base/application/adapters/s3.adapter';
+import { BlogsRepository } from '../../infrastructure/blogs.repository';
+
+export class BlogAddMainImageCommand {
+  constructor(
+    public blogId: string,
+    public userId: string,
+    public buffer: Buffer,
+    public mimetype: string,
+    public originalName: string,
+  ) {}
+}
+
+@CommandHandler(BlogAddMainImageCommand)
+export class BlogAddMainImageUseCase
+  implements ICommandHandler<BlogAddMainImageCommand>
+{
+  constructor(
+    private readonly blogsQueryRepository: BlogsQueryRepository,
+    private readonly blogsRepository: BlogsRepository,
+    private readonly s3Adapter: S3Adapter,
+  ) {}
+
+  async execute(
+    command: BlogAddMainImageCommand,
+  ): Promise<ExceptionResultType<boolean>> {
+    const blog = await this.blogsQueryRepository.findBlogEntity(
+      +command.blogId,
+    );
+
+    if (!blog) {
+      return {
+        data: false,
+        code: ResultCode.NotFound,
+        field: blogIdField,
+        message: blogNotFound,
+      };
+    }
+
+    if (blog.user.id !== +command.userId) {
+      return {
+        data: false,
+        code: ResultCode.Forbidden,
+      };
+    }
+
+    const s3Key = `blogger/images/main/${command.blogId}_${command.originalName}`;
+    await this.s3Adapter.uploadImage(s3Key, command.buffer, command.mimetype);
+
+    const image = sharp(command.buffer);
+    const metadata = await image.metadata();
+
+    const result = await this.blogsRepository.uploadBlogMainImage(
+      metadata,
+      s3Key,
+      blog,
+    );
+
+    if (!result) {
+      return {
+        data: false,
+        code: ResultCode.NotFound,
+        field: blogIdField,
+        message: blogNotFound,
+      };
+    }
+
+    return {
+      data: true,
+      code: ResultCode.Success,
+    };
+  }
+}
