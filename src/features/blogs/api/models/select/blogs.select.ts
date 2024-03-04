21229cc08f8f1c8320ff5fc6a39ee8eb117bd7fb
Denis Gamayunov <gamaunov1911@gmail.com>
2024-03-04 08:39:51 +0300

    fix IBlogsSelect

--- src/features/blogs/api/models/select/blogs.select.ts
+++ src/features/blogs/api/models/select/blogs.select.ts
@@ -5,4 +5,18 @@ export interface IBlogsSelect {
   websiteUrl: string;
   createdAt: Date;
   isMembership: boolean;
+  bwi_url: string | null;
+  bwi_width: number | null;
+  bwi_height: number | null;
+  bwi_size: number | null;
+  mainImages:
+    | [
+        {
+          url: string;
+          width: number;
+          height: number;
+          size: number;
+        },
+      ]
+    | [];
 }
