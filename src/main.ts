faf193a7ce680ff5289913276f972918f2e49836
Gamaunov <gamaunov1911@gmail.com>
2024-03-11 20:54:03 +0300

    bootstrap: add envConfig

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,36 @@
 import { NestFactory } from '@nestjs/core';
-import * as process from 'process';
-import * as ngrok from 'ngrok';
+import { Logger } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { APP_PREFIX, applyAppSettings } from './settings/apply-app-setting';
 import { TelegramAdapter } from './features/integrations/telegram/adapters/telegram.adapter';
+import { envConfig } from './settings/env.config';
 
-const PORT = parseInt(process.env.PORT, 10) || 5000;
+const PORT = envConfig.PORT || 5000;
 
-let hookUrl = process.env.APP_BASE_URL;
-async function connectToNgrok() {
-  return ngrok.connect(5000);
-}
+const hookUrl = envConfig.NGROK.URL;
+
+// async function connectToNgrok() {
+//   return ngrok.connect(5000);
+// }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger();
 
   applyAppSettings(app);
 
   await app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}${APP_PREFIX}`, process.env.ENV);
+    if (envConfig.ENV === 'DEVELOPMENT') {
+      logger.log(`http://localhost:${PORT}${APP_PREFIX}`, envConfig.ENV);
+    }
   });
 
   const telegramAdapter = await app.resolve(TelegramAdapter);
 
-  if (process.env.NODE_ENV === 'development') {
-    hookUrl = await connectToNgrok();
-  }
+  // if (process.env.ENV === 'DEVELOPMENT') {
+  //   hookUrl = await connectToNgrok();
+  // }
 
   await telegramAdapter.setWebhook(hookUrl + 'integrations/telegram/webhook');
 }
