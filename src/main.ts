85887f182d2f7060e9dd72c17d8be3532e17f274
Gamaunov <gamaunov1911@gmail.com>
2024-03-16 15:48:05 +0300

    fix main

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import process from 'process';
 
 import { AppModule } from './app.module';
 import { APP_PREFIX, applyAppSettings } from './settings/apply-app-setting';
 import { TelegramAdapter } from './features/integrations/telegram/adapters/telegram.adapter';
-import { envConfig } from './settings/env.config';
 
-const PORT = envConfig.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-const hookUrl = envConfig.NGROK.URL;
+const hookUrl = process.env.APP_BASE_URL;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,8 +17,8 @@ async function bootstrap() {
   applyAppSettings(app);
 
   await app.listen(PORT, () => {
-    if (envConfig.ENV === 'DEVELOPMENT') {
-      logger.log(`http://localhost:${PORT}${APP_PREFIX}`, envConfig.ENV);
+    if (process.env.ENV === 'DEVELOPMENT') {
+      logger.log(`http://localhost:${PORT}${APP_PREFIX}`, process.env.ENV);
     }
   });
 
