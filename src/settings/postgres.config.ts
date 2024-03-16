0744274e7bb8c00e4bb93f71c1ac7c2414295776
Gamaunov <gamaunov1911@gmail.com>
2024-03-16 15:36:54 +0300

    fix entities

--- src/settings/postgres.config.ts
+++ src/settings/postgres.config.ts
@@ -1,6 +1,25 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-import { appEntities } from '../base/application/domain/app.entities';
+import { User } from '../features/users/domain/user.entity';
+import { Blog } from '../features/blogs/domain/blog.entity';
+import { CommentLike } from '../features/comments/domain/comment-like.entity';
+import { Comment } from '../features/comments/domain/comment.entity';
+import { DeviceAuthSessions } from '../features/devices/domain/device.entity';
+import { Post } from '../features/posts/domain/post.entity';
+import { PostLike } from '../features/posts/domain/post-like.entity';
+import { UserEmailConfirmation } from '../features/users/domain/user-email-confirmation.entity';
+import { UserPasswordRecovery } from '../features/users/domain/user-password-recovery.entity';
+import { QuizAnswer } from '../features/quiz/domain/quiz-answer.entity';
+import { QuizGame } from '../features/quiz/domain/quiz-game.entity';
+import { QuizPlayer } from '../features/quiz/domain/quiz-player';
+import { QuizQuestion } from '../features/quiz/domain/quiz-question.entity';
+import { UserBanInfo } from '../features/users/domain/user-ban.entity';
+import { UserBanByBlogger } from '../features/users/domain/user-ban-by-blogger.entity';
+import { BlogBan } from '../features/blogs/domain/blog-ban.entity';
+import { BlogMainImage } from '../features/blogs/domain/blog-main-image.entity';
+import { BlogWallpaperImage } from '../features/blogs/domain/blog-wallpaper-image.entity';
+import { PostMainImage } from '../features/posts/domain/post-main-image.entity';
+import { TgBlogSubscriber } from '../features/integrations/telegram/domain/tg.blog.subscriber.entity';
 
 import { envConfig } from './env.config';
 
@@ -15,7 +34,28 @@ export const postgresConfig: TypeOrmModuleOptions = {
   synchronize: true,
   logging: 'all',
   logger: 'debug',
-  entities: [...appEntities],
+  entities: [
+    User,
+    Blog,
+    CommentLike,
+    Comment,
+    DeviceAuthSessions,
+    Post,
+    PostLike,
+    UserEmailConfirmation,
+    UserPasswordRecovery,
+    QuizAnswer,
+    QuizGame,
+    QuizPlayer,
+    QuizQuestion,
+    UserBanInfo,
+    UserBanByBlogger,
+    BlogBan,
+    BlogMainImage,
+    BlogWallpaperImage,
+    PostMainImage,
+    TgBlogSubscriber,
+  ],
   ssl: true,
   extra: {
     ssl: {
--- typeorm.config.ts
+++ typeorm.config.ts
@@ -2,7 +2,26 @@ import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
 
 import { envConfig } from './src/settings/env.config';
-import { appEntities } from './src/base/application/domain/app.entities';
+import { User } from './src/features/users/domain/user.entity';
+import { Blog } from './src/features/blogs/domain/blog.entity';
+import { CommentLike } from './src/features/comments/domain/comment-like.entity';
+import { Comment } from './src/features/comments/domain/comment.entity';
+import { DeviceAuthSessions } from './src/features/devices/domain/device.entity';
+import { Post } from './src/features/posts/domain/post.entity';
+import { PostLike } from './src/features/posts/domain/post-like.entity';
+import { UserEmailConfirmation } from './src/features/users/domain/user-email-confirmation.entity';
+import { UserPasswordRecovery } from './src/features/users/domain/user-password-recovery.entity';
+import { QuizAnswer } from './src/features/quiz/domain/quiz-answer.entity';
+import { QuizGame } from './src/features/quiz/domain/quiz-game.entity';
+import { QuizPlayer } from './src/features/quiz/domain/quiz-player';
+import { QuizQuestion } from './src/features/quiz/domain/quiz-question.entity';
+import { UserBanInfo } from './src/features/users/domain/user-ban.entity';
+import { UserBanByBlogger } from './src/features/users/domain/user-ban-by-blogger.entity';
+import { BlogBan } from './src/features/blogs/domain/blog-ban.entity';
+import { BlogMainImage } from './src/features/blogs/domain/blog-main-image.entity';
+import { BlogWallpaperImage } from './src/features/blogs/domain/blog-wallpaper-image.entity';
+import { PostMainImage } from './src/features/posts/domain/post-main-image.entity';
+import { TgBlogSubscriber } from './src/features/integrations/telegram/domain/tg.blog.subscriber.entity';
 
 config();
 
@@ -14,5 +33,26 @@ export default new DataSource({
   password: envConfig.DB.POSTGRES.PASSWORD,
   database: envConfig.DB.POSTGRES.DATABASE_NAME,
   migrations: ['migrations/*.ts'],
-  entities: [...appEntities],
+  entities: [
+    User,
+    Blog,
+    CommentLike,
+    Comment,
+    DeviceAuthSessions,
+    Post,
+    PostLike,
+    UserEmailConfirmation,
+    UserPasswordRecovery,
+    QuizAnswer,
+    QuizGame,
+    QuizPlayer,
+    QuizQuestion,
+    UserBanInfo,
+    UserBanByBlogger,
+    BlogBan,
+    BlogMainImage,
+    BlogWallpaperImage,
+    PostMainImage,
+    TgBlogSubscriber,
+  ],
 });
