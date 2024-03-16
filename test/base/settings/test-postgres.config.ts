5431087d4fd795f4c41e872cf1be3a7f9d7353ea
Gamaunov <gamaunov1911@gmail.com>
2024-03-16 15:47:06 +0300

    fix testPostgresConfig

--- test/base/settings/test-postgres.config.ts
+++ test/base/settings/test-postgres.config.ts
@@ -1,20 +1,60 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as process from 'process';
 
-import { appEntities } from '../../../src/base/application/domain/app.entities';
+import { User } from '../../../src/features/users/domain/user.entity';
+import { Blog } from '../../../src/features/blogs/domain/blog.entity';
+import { CommentLike } from '../../../src/features/comments/domain/comment-like.entity';
+import { Comment } from '../../../src/features/comments/domain/comment.entity';
+import { DeviceAuthSessions } from '../../../src/features/devices/domain/device.entity';
+import { Post } from '../../../src/features/posts/domain/post.entity';
+import { PostLike } from '../../../src/features/posts/domain/post-like.entity';
+import { UserEmailConfirmation } from '../../../src/features/users/domain/user-email-confirmation.entity';
+import { UserPasswordRecovery } from '../../../src/features/users/domain/user-password-recovery.entity';
+import { QuizAnswer } from '../../../src/features/quiz/domain/quiz-answer.entity';
+import { QuizGame } from '../../../src/features/quiz/domain/quiz-game.entity';
+import { QuizPlayer } from '../../../src/features/quiz/domain/quiz-player';
+import { QuizQuestion } from '../../../src/features/quiz/domain/quiz-question.entity';
+import { UserBanInfo } from '../../../src/features/users/domain/user-ban.entity';
+import { UserBanByBlogger } from '../../../src/features/users/domain/user-ban-by-blogger.entity';
+import { BlogBan } from '../../../src/features/blogs/domain/blog-ban.entity';
+import { BlogMainImage } from '../../../src/features/blogs/domain/blog-main-image.entity';
+import { BlogWallpaperImage } from '../../../src/features/blogs/domain/blog-wallpaper-image.entity';
+import { PostMainImage } from '../../../src/features/posts/domain/post-main-image.entity';
+import { TgBlogSubscriber } from '../../../src/features/integrations/telegram/domain/tg.blog.subscriber.entity';
 
 export const testPostgresConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.TEST_POSTGRES_HOST,
+  host: process.env.POSTGRES_HOST,
   port: 5432,
-  username: process.env.TEST_POSTGRES_USER,
-  password: process.env.TEST_POSTGRES_PASSWORD,
-  database: process.env.TEST_POSTGRES_DATABASE,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
   autoLoadEntities: false,
   synchronize: false,
   logging: 'all',
   logger: 'debug',
-  entities: [...appEntities],
+  entities: [
+    User,
+    Blog,
+    CommentLike,
+    Comment,
+    DeviceAuthSessions,
+    Post,
+    PostLike,
+    UserEmailConfirmation,
+    UserPasswordRecovery,
+    QuizAnswer,
+    QuizGame,
+    QuizPlayer,
+    QuizQuestion,
+    UserBanInfo,
+    UserBanByBlogger,
+    BlogBan,
+    BlogMainImage,
+    BlogWallpaperImage,
+    PostMainImage,
+    TgBlogSubscriber,
+  ],
   ssl: true,
   extra: {
     ssl: {
